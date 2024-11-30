c44b154f03a8235023e4cad023458c0a03e3fb90
Nuzup Shadiev <nuzupshadiev@gmail.com>
2024-11-30 15:42:30 +0900

    GPT

--- GA/llm_api.py
+++ GA/llm_api.py
@@ -1,29 +1,40 @@
 import os
 import openai
 from dotenv import load_dotenv
+from openai import OpenAI
 
 # Load the .env file
 load_dotenv()
-
 # Get the API key from the .env file
-api_key = os.getenv('LLM_API_KEY')
-
+api_key = os.getenv("LLM_API_KEY")
 # Set the API key
 openai.api_key = api_key
 
-def call_openai_api(prompt, model="gpt-4-turbo"):
+print("OpenAI API Key: ", api_key)
+client = OpenAI(
+    api_key=api_key,
+)
+
+
+def call_openai_api(prompt, model="gpt-4o-mini"):
     try:
-        response = openai.ChatCompletion.create(
+        response = client.chat.completions.create(
             model=model,
             messages=[
-                {"role": "system", "content": "You are a helpful assistant for writing code."},
+                {
+                    "role": "system",
+                    "content": "You are a helpful assistant for writing code.",
+                },
                 {"role": "user", "content": prompt},
             ],
-            max_tokens=1000,
-            temperature=0.5,
         )
         # TODO: parse the only code section from the response
-        return response['choices'][0]['message']['content'].strip()
+        return response["choices"][0]["message"]["content"].strip()
     except Exception as e:
         print("Error on openai.ChatCompletion.create: ", e)
-    
+
+
+if __name__ == "__main__":
+    call_openai_api(
+        "Write a Python function that takes a list of numbers and returns the sum of the list."
+    )
